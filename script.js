13fe568766e69a8a0b26f70e9bd31961bc911d34
ayninur <nuralaynabdulqadir@gmail.com>
2020-11-27 16:32:47 -0500

    basic html&css complete selected elements to be used DOM

---
+++ script.js
@@ -0,0 +1,17 @@
+const search = document.getElementById('search'),
+    submit = document.getElementById('submit'),
+    weekly = document.getElementById('weekly'),
+    random = document.getElementById('random'),
+    mealsElement = document.getElementById('meals'),
+    resultHeading = document.getElementById('result-heading'),
+    singleMealElement = document.getElementById('single-meal');
+
+
+//functions/event handles
+
+//search meal and fetch from api
+
+
+
+//Event listeners
+submit.addEventListener('submit', searchMeal);
